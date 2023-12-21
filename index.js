2772378bd1e4eadaa89a34a8f040a861a65bca6f
Alphie ochieng <24778809+alphieoch@users.noreply.github.com>
2023-12-21 15:45:20 +0000

    Removing the non essentials

--- index.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer (function (req, res) {
-res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-res.end ('Hello World!');
-}). listen(8000, function () {
-console.log('Node server is running….');
-});
